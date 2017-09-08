358f63a4a07d84b9cc817282f1f442598195c8a9
Mark A Lang <mlang12@Marks-MacBook-Pro.local>
2017-09-07 20:06:13 -0400

    added placeholder files and added sign-up code

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,8 @@ import Profile from './components/Profile.js';
 import Signin from './components/Signin.js';
 import Signup from './components/Signup.js';
 import Header from './components/Header.js';
+import Liked from './components/Liked.js';
+import Filters from './components/Filters.js';
 import Gameview from './components/Gameview.js';
 import logo from './logo.svg';
 import './App.css';
@@ -64,14 +66,20 @@ class App extends Component {
         </div>
         <Switch>
           <Route exact path='/' component={Home}/>
-          {/*<Route exact path='/signin' component={Signin} setAuth={this.setAuth}/>*/}
-          {/*<Route exact path='/signup' component={Signup} setAuth={this.setAuth}/>*/}
-          <PrivateRoute exact path='/profile' component={Profile} />
+          <Route exact path='/signup' render={(props) => (
+            <Signup {...props} isAuth={isAuth}/>
+          )}/>
+          <Route exact path='/profile' component={Profile} />
+          <Route exact path='/likes' component={Liked} />
+          <Route exact path='/filters' component={Filters} />
           <Route path="/gameview" component={Gameview} />
           <Route exact path='/signin' render={(props) => (
             <Signin {...props} isAuth={isAuth}/>
           )}/>
         </Switch>
+        <div className="headerArea">
+          <Header isAuth={this.state.authState}/>
+        </div>
       </div>
     );
   }
---
+++ app/src/components/Liked.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Liked extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount(){
+  }
+  render() {
+    return (
+      <div className="landingTileArea">
+        <h1>Liked Will Go Here</h1>
+      </div>
+    );
+  }
+}
+
+export default Liked;
